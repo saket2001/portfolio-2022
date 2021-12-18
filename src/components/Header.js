6a44f9c6d70b3f33bc3e0c60b3be8f0b5ac67eee
saket2001 <saketchandorkar123@gmail.com>
2021-12-18 10:47:15 +0530

    removed unused vars

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { gsap, Power3, SteppedEase } from "gsap/all";
+import { gsap, Power3 } from "gsap/all";
 
 export const Header = () => {
   const textItem = useRef();
